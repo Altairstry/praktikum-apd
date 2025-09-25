cd765d0df8b0a7dcb088cc5b3e7a3045832def33
Altairstry <pirlosyabilahafuza@gmail.com>
2025-09-25 13:54:36 +0800

    selesai

--- A-1/pertemuan-2/main.py
+++
@@ -1,8 +0,0 @@
-nilai = 70
-
-if nilai > 75:
-    print("Nilai A")
-elif nilai > 65:
-    print("Nilai B")
-else:
-    print("Nilai C")
---
+++ A-1/pertemuan-3/main.py
@@ -0,0 +1,41 @@
+# nilai = 70
+# if nilai > 75:
+#     print("Nilai A")
+# elif nilai > 65:
+#     print("Nilai B")
+# else:
+#     print("Nilai C")
+
+# suhu = 35
+# if suhu >= 35:
+#     print("Panas")
+# if suhu > 30:
+#     print("Sauna")
+# elif suhu > 20:
+#     print("Normal")
+# else:
+#     print("Dingin")
+
+# umur = int(input("Masukkan umur anda"))
+# if umur < 13:
+#     kategori = "anak-anak"
+# elif umur < 18:
+#     kategori = "remaja"
+# elif umur < 60:
+#     kategori = "dewasa"
+# else :
+#     kategori = "lansia"
+
+# print("umur:",umur, "kategori:", kategori)
+
+# nilai = 50
+
+# status = "lulus" if nilai >= 60 else "tidak lulus"
+# print(status)
+
+# tinggi = 145
+
+# if tinggi >= 145:
+#     print("boleh masuk")
+# else:
+#     print("tidak boleh masuk")
