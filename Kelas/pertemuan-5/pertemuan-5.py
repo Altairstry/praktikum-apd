8c94e58b17e44112cdc39f0516f31d4d809bfb99
Altairstry <pirlosyabilahafuza@gmail.com>
2025-10-09 14:31:47 +0800

    done

--- Kelas/pertemuan-5/pertemuan-5.py
+++ Kelas/pertemuan-5/pertemuan-5.py
@@ -1,14 +1,32 @@
-praktikum = ["APD", "Orsikom", "Jarkom", "RPL", ]
+tugas = ("rangkuman", "arduino", "scrapping", "matriks", "visualisasi")
+(PTI, orsikom, basisdata, *manipulasiData) = tugas
+print(manipulasiData)
 
+#tuple menggunakan kurung dan hanya perlu menambahkan koma untuk dianggap tuple
+# aslab = ["harahap", "denny"]
+# matkul = ("apd", "matdis")
+# listmatkul = list(matkul)
+# kombinasi = aslab + listmatkul 
+# print(kombinasi)
+
+
+
+
+
+#i mengakses list pertama dan j mengakses list kedua jika ada list lagi dalam list ke dua maka looping akan ditambah
+# mahasiswa = [["Yoga", "Dante", "Santoso"], ["Pernanda", "Trivia", "Ahnaf"]]
+# for i in mahasiswa:
+#     for j in i :
+#         print (j)
 
 # print(praktikum)
-
-angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-huruf = ["a", "b", "c"]
-campur = huruf * 3
-print(campur)
+# angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+# huruf = ["a", "b", "c"]
+# campur = huruf * 3
+# print(campur)
 # print(tes[0: 10: 1])
 
+# praktikum = ["APD", "Orsikom", "Jarkom", "RPL", ]
 # praktikum.remove("APD")
 # del praktikum[2]
 # buang = praktikum.pop()
