e58379ac8212a0b200dfc4a4e64e69046011b366
Altairstry <pirlosyabilahafuza@gmail.com>
2025-10-09 13:56:16 +0800

    belajar

---
+++ Kelas/pertemuan-5/pertemuan-5.py
@@ -0,0 +1,29 @@
+praktikum = ["APD", "Orsikom", "Jarkom", "RPL", ]
+
+
+# print(praktikum)
+
+angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+huruf = ["a", "b", "c"]
+campur = huruf * 3
+print(campur)
+# print(tes[0: 10: 1])
+
+# praktikum.remove("APD")
+# del praktikum[2]
+# buang = praktikum.pop()
+# inputmatkul = input("Matkul : ")
+# praktikum.append(inputmatkul)
+# print(praktikum)
+# print(praktikum)
+# praktikum[2]= "Kalkulus"
+# praktikum.insert(2, "Strukdat")
+# praktikum.append("Basdat")
+
+
+
+# print(praktikum[1][1])
+
+# print(praktikum)
+# for i in praktikum:
+#     print(i)
